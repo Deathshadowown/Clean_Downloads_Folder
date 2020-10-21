3048c08fea1300c76aace4f6b8953f5cb8cbd922
Deathshadowown <shanervanniekerk@gmail.com>
2020-10-21 22:44:50 +0200

    updated run file to make jar file

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 rm -f src/*.class
+rm -f myJarFile.jar;
 javac src/Main.java
 java src/Main
-rm -f src/*.class #remove after testing
+# rm -f src/*.class #remove after testing
+#creating jar file
+jar -cvf myJarFile.jar src/Main.class
+#Execute the java file
+# java -cp MyJarFile.jar src/Main
